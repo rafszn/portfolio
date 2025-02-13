9d6637fc67c61c4ac0257b8e429546ae32edf9d9
rafael <yhrapha@gmail.com>
2025-02-13 16:11:33 +0100

    vercel.json

--- src/components/sections/ContactSection.jsx
+++ src/components/sections/ContactSection.jsx
@@ -5,18 +5,6 @@ import { useState } from "react";
 gsap.registerPlugin(useGSAP);
 
 const ContactSection = () => {
-  useGSAP(() => {
-    // gsap.to(".image-wrapper", {
-    //   duration: 3,
-    //   keyframes: [
-    //     // { borderRadius: "80% 50% 70% 20% / 100% 70% 40% 30%" },
-    //     { borderRadius: "100% 50% 70% 75% / 100% 100% 50% 60%" },
-    //     { borderRadius: "80% 50% 70% 20% / 100% 70% 40% 30%" },
-    //   ],
-    //   repeat: -1, // Repeat indefinitely
-    //   yoyo: true, // Reverse the animation on every alternate cycle
-    // });
-  });
   return (
     <div className="contact-section">
       <div className="wrapper">
